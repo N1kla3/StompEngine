8d6a010b7b73fd0da3a6616a857db170af2d9528
ALEXMRXIT <66775684+ALEXMRXIT@users.noreply.github.com>
2025-11-25 16:58:16 +0300

    add STOMP_API export macros for Windows and Unix

--- Engine/CoreModule/src/Core/Defines.h
+++ Engine/CoreModule/src/Core/Defines.h
@@ -1,4 +1,17 @@
 #pragma once
 
-// TODO win64
-#define STOMP_API __attribute__((visibility("default")))
+#if defined(_WIN32) || defined(_WIN64)
+    #ifdef STOMP_BUILD_API
+        #define STOMP_API __declspec(dllexport)
+    #elif defined(STOMP_USE_API)
+        #define STOMP_API __declspec(dllimport)
+    #else
+        #define STOMP_API
+    #endif
+#else
+    #if __GNUC__ >= 4
+        #define STOMP_API __attribute__((visibility("default")))
+    #else
+        #define STOMP_API
+    #endif
+#endif
